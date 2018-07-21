6bfff5e0d7ebce203e82d9a077ccde3a621d3160
wh0am123 <thejyjco@gmail.com>
2018-07-21 13:06:59 -0430

    Juan was here

--- js/busqueda/searcher/index.js
+++ js/busqueda/searcher/index.js
@@ -4,7 +4,18 @@ var ajax = new XMLHttpRequest();
 function chargeEvents() {
 	ajax.onreadystatechange = function() {
 		if(ajax.readyState == 4 && this.status == 200) {
-		    content.innerHTML = this.responseText;
+		    var users = JSON.parse(this.responseText);
+		    var hots = [];
+		    users.map(function(user) {
+		 		var item = '<div class="item item-hot" style="background-image: url(../Perfil/'+user.perfil+');background-size: cover;background-repeat: no-repeat;background-position: center;display: flex;flex-direction: column;justify-content: flex-end;"><div class="container" style="margin: 4%;text-align: left;color: #fff;"><h2><a href="#" class="link" style="color: #fff;">'+user.nombre+'</a><br><small><a href="#" class="link" style="color: #fff;">@'+user.direccion+'</a></small></h2></div></div>'
+		 		hots.push(item);
+		    });
+		    var hotContent = '';
+		  	hots.forEach(function(hot) {
+		  		 hotContent += hot;
+		  	});
+		  	var content = document.getElementById('content');
+		  	content.innerHTML = hotContent;
 		}
 	};
 	ajax.open('get', '../php/ajax/search-hot.php');
--- php/ajax/search-hot.php
+++ php/ajax/search-hot.php
@@ -1,5 +1,15 @@
 <?php 
 	include '../class/ConnectionClass.php';
-	$conn = 
-	echo "Hello, World!";
+	$conn = new Connection();
+	$test = $conn->connect();
+	$sel = $conn->select_db($test, 'fluxer');
+	$sql = "SELECT * FROM usuarios";
+	$query = mysqli_query($test, $sql);
+	$users = array();
+	while($row = mysqli_fetch_array($query, MYSQLI_BOTH)) {
+		$users_array = array('perfil' => $row['F_PERFIL'], 'nombre' => $row['NOMBRE_USUARIO'], 'direccion' => $row['DIRECCION'], 'link' => $row['LINK']);
+		array_push($users, $users_array);
+	}
+	$users_json = json_encode($users);
+	echo $users_json;
  ?>
